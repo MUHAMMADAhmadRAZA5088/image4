9ffe4d17a5334dd81f8a3cac5e644627211982f6
MUHAMMADAhmadRAZA5088 <99063548+MUHAMMADAhmadRAZA5088@users.noreply.github.com>
2022-08-09 23:23:45 +0500

    Update settings.py

--- E_comrance/settings.py
+++ E_comrance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zh@unul9bc$oyolq-z&!&66s3-mmxg-bm#kcmu2ytb3v_*i4bq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['image3.herokuapp.com']
 
 
 # Application definition
